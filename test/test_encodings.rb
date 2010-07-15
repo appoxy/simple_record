1bd83c09538c358af024e4e1df3e35dc249dbf37
Travis Reeder <travis@appoxy.com>
2010-07-15 11:42:21 -0700

    Added encoding tests from Chris Finne: http://gist.github.com/364074

--- test/test_encodings.rb
+++ test/test_encodings.rb
@@ -14,5 +14,30 @@ class Person < SimpleRecord::Base
 end
 class TestEncodings < TestBase
 
-    
+    def test_ascii_http_post
+        first_name = "joe" + ("X" * 1000) # pad the field to help get the URL over the 2000 length limit so AWS uses a POST
+        last_name = "blow" + ("X" * 1000) # pad the field to help get the URL over the 2000 length limit so AWS uses a POST
+        mm = MyModel.create :first_name=>first_name, :last_name=>last_name
+        mm.save
+        sleep 1
+        assert mm.first_name == first_name
+        assert mm.last_name == last_name
+        mm2 = MyModel.find(mm.id)
+        assert mm2.first_name == first_name
+        assert mm2.last_name == last_name
+    end
+
+    def test_utf8_http_post
+        first_name = "josé" + ("X" * 1000) # pad the field to help get the URL over the 2000 length limit so AWS uses a POST
+        last_name = "??" + ("X" * 1000) # pad the field to help get the URL over the 2000 length limit so AWS uses a POST
+        mm = MyModel.create :first_name=>first_name, :last_name=>last_name
+        mm.save
+        sleep 1
+        assert mm.first_name == first_name
+        assert mm.last_name == last_name
+        mm2 = MyModel.find(mm.id)
+        assert mm2.first_name == first_name
+        assert mm2.last_name == last_name
+    end
+
 end
