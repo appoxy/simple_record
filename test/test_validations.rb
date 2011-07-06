9ed5b455611093152eb1119f2ab3c2f9d0b4fc99
Travis Reeder <travis@appoxy.com>
2011-07-06 00:51:28 -0700

    put test back in.

--- test/test_validations.rb
+++ test/test_validations.rb
@@ -15,45 +15,45 @@ require_relative 'models/validated_model'
 
 class TestValidations < TestBase
 
-  #
-  #def test_validations
-  #  mm = MyModel.new()
-  #  puts 'invalid? ' + mm.invalid?.to_s
-  #  assert mm.invalid?, "mm is valid. invalid? returned #{mm.invalid?}"
-  #  assert mm.errors.size == 1
-  #  assert !mm.attr_before_create
-  #  assert !mm.valid?
-  #  assert mm.save == false, mm.errors.inspect
-  #  assert !mm.attr_before_create # should not get called if not valid
-  #  assert !mm.attr_after_save
-  #  assert !mm.attr_after_create
-  #  mm.name = "abcd"
-  #  assert mm.valid?, mm.errors.inspect
-  #  assert mm.errors.size == 0
-  #
-  #  mm.save_count = 2
-  #  assert mm.invalid?
-  #
-  #  mm.save_count = nil
-  #  assert mm.valid?
-  #  assert mm.save, mm.errors.inspect
-  #
-  #  p mm
-  #  assert mm.attr_before_create
-  #  assert mm.attr_after_save
-  #  assert mm.attr_after_create
-  #  assert !mm.attr_after_update
-  #
-  #  assert mm.valid?, mm.errors.inspect
-  #  assert mm.save_count == 1
-  #
-  #  mm.name = "abc123"
-  #  assert mm.save
-  #
-  #  assert mm.attr_after_update
-  #
-  #end
-  #
+  
+  def test_validations
+    mm = MyModel.new()
+    puts 'invalid? ' + mm.invalid?.to_s
+    assert mm.invalid?, "mm is valid. invalid? returned #{mm.invalid?}"
+    assert mm.errors.size == 1
+    assert !mm.attr_before_create
+    assert !mm.valid?
+    assert mm.save == false, mm.errors.inspect
+    assert !mm.attr_before_create # should not get called if not valid
+    assert !mm.attr_after_save
+    assert !mm.attr_after_create
+    mm.name = "abcd"
+    assert mm.valid?, mm.errors.inspect
+    assert mm.errors.size == 0
+
+    mm.save_count = 2
+    assert mm.invalid?
+
+    mm.save_count = nil
+    assert mm.valid?
+    assert mm.save, mm.errors.inspect
+
+    p mm
+    assert mm.attr_before_create
+    assert mm.attr_after_save
+    assert mm.attr_after_create
+    assert !mm.attr_after_update
+
+    assert mm.valid?, mm.errors.inspect
+    assert mm.save_count == 1
+
+    mm.name = "abc123"
+    assert mm.save
+
+    assert mm.attr_after_update
+
+  end
+
 
   def test_more_validations
 
