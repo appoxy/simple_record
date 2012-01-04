f951df1318f1a6bc6dd95ea0ec42ac93fcc4b89c
Chuck LeDuc Díaz <celeduc@gmail.com>
2012-01-04 12:43:01 +0100

    1. Don't sleep in tests. Use ConsistentRead. 2. Clean up beforehand. Clean up afterward. 3. Test delete, too. Why not.

--- test/test_encodings.rb
+++ test/test_encodings.rb
@@ -2,42 +2,42 @@ require 'test/unit'
 require File.expand_path(File.dirname(__FILE__) + "/../lib/simple_record")
 require "yaml"
 require 'aws'
-require 'my_model'
-require 'my_child_model'
+require 'my_simple_model'
 require 'active_support'
 require 'test_base'
 
 
-class Person < SimpleRecord::Base
-    has_strings :name, :i_as_s
-    has_ints :age
-end
 class TestEncodings < TestBase
 
+    def test_setup_delete_domain
+        MySimpleModel.delete_domain
+    end
     def test_ascii_http_post
         name = "joe" + ("X" * 1000) # pad the field to help get the URL over the 2000 length limit so AWS uses a POST
         nickname = "blow" + ("X" * 1000) # pad the field to help get the URL over the 2000 length limit so AWS uses a POST
-        mm = MyModel.create :name=>name, :nickname=>nickname
-        mm.save
-        sleep 1
+        mm = MySimpleModel.create :name=>name, :nickname=>nickname
+        assert mm.save
         assert mm.name == name
         assert mm.nickname == nickname
-        mm2 = MyModel.find(mm.id)
+        mm2 = MySimpleModel.find(mm.id,:consistent_read=>true)
         assert mm2.name == name
         assert mm2.nickname == nickname
+        assert mm2.delete
     end
 
     def test_utf8_http_post
         name = "jos\u00E9" + ("X" * 1000) # pad the field to help get the URL over the 2000 length limit so AWS uses a POST
         nickname = "??" + ("X" * 1000) # pad the field to help get the URL over the 2000 length limit so AWS uses a POST
-        mm = MyModel.create :name=>name, :nickname=>nickname
-        mm.save
-        sleep 1
+        mm = MySimpleModel.create :name=>name, :nickname=>nickname
+        assert mm.save
         assert mm.name == name
         assert mm.nickname == nickname
-        mm2 = MyModel.find(mm.id)
+        mm2 = MySimpleModel.find(mm.id,:consistent_read=>true)
         assert mm2.name == name
         assert mm2.nickname == nickname
+        assert mm2.delete
     end
-
+    def test_cleanup_delete_domain
+        MySimpleModel.delete_domain
+    end
 end
