64368b106d7032fe2c677ca08bf66bca8fec9f3e
Chuck LeDuc Díaz <celeduc@gmail.com>
2012-01-04 12:22:42 +0100

    Fix test where encoding was borked and the properties in the class being used had been renamed.  <Sheesh>

--- test/test_encodings.rb
+++ test/test_encodings.rb
@@ -15,29 +15,29 @@ end
 class TestEncodings < TestBase
 
     def test_ascii_http_post
-        first_name = "joe" + ("X" * 1000) # pad the field to help get the URL over the 2000 length limit so AWS uses a POST
-        last_name = "blow" + ("X" * 1000) # pad the field to help get the URL over the 2000 length limit so AWS uses a POST
-        mm = MyModel.create :first_name=>first_name, :last_name=>last_name
+        name = "joe" + ("X" * 1000) # pad the field to help get the URL over the 2000 length limit so AWS uses a POST
+        nickname = "blow" + ("X" * 1000) # pad the field to help get the URL over the 2000 length limit so AWS uses a POST
+        mm = MyModel.create :name=>name, :nickname=>nickname
         mm.save
         sleep 1
-        assert mm.first_name == first_name
-        assert mm.last_name == last_name
+        assert mm.name == name
+        assert mm.nickname == nickname
         mm2 = MyModel.find(mm.id)
-        assert mm2.first_name == first_name
-        assert mm2.last_name == last_name
+        assert mm2.name == name
+        assert mm2.nickname == nickname
     end
 
     def test_utf8_http_post
-        first_name = "josé" + ("X" * 1000) # pad the field to help get the URL over the 2000 length limit so AWS uses a POST
-        last_name = "??" + ("X" * 1000) # pad the field to help get the URL over the 2000 length limit so AWS uses a POST
-        mm = MyModel.create :first_name=>first_name, :last_name=>last_name
+        name = "jos\u00C9" + ("X" * 1000) # pad the field to help get the URL over the 2000 length limit so AWS uses a POST
+        nickname = "??" + ("X" * 1000) # pad the field to help get the URL over the 2000 length limit so AWS uses a POST
+        mm = MyModel.create :name=>name, :nickname=>nickname
         mm.save
         sleep 1
-        assert mm.first_name == first_name
-        assert mm.last_name == last_name
+        assert mm.name == name
+        assert mm.nickname == nickname
         mm2 = MyModel.find(mm.id)
-        assert mm2.first_name == first_name
-        assert mm2.last_name == last_name
+        assert mm2.name == name
+        assert mm2.nickname == nickname
     end
 
 end
